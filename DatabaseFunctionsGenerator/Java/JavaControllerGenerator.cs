fa238ae6bda900336553ccae218db87bd46d69c0
biabeniamin <biabeniamin@gmail.com>
2018-10-16 22:23:32 +0300

    generates async method to get all data

--- DatabaseFunctionsGenerator/Java/JavaControllerGenerator.cs
+++ DatabaseFunctionsGenerator/Java/JavaControllerGenerator.cs
@@ -182,6 +182,91 @@ namespace DatabaseFunctionsGenerator.Java
             return builder.ToString();
         }
 
+        private string GenerateAsyncGetMethod(Table table)
+        {
+            StringBuilder builder;
+            StringBuilder methodBody;
+
+            builder = new StringBuilder();
+            methodBody = new StringBuilder();
+
+            builder.AppendLine($"public static void get{table.Name}(Call<List<{table.SingularName}>> call, Callback<List<{table.SingularName}>> callback)");
+            builder.AppendLine("{");
+            {
+                //declaration
+
+                //initialization
+
+                //try
+                methodBody.AppendLine("try");
+                methodBody.AppendLine("{");
+                {
+                    //get data from server
+                    methodBody.AppendLine($"\tcall.enqueue(callback);");
+                }
+                methodBody.AppendLine("}");
+
+                //catch
+                methodBody.AppendLine("catch(Exception ee)");
+                methodBody.AppendLine("{");
+                {
+                    methodBody.AppendLine($"\tSystem.out.println(ee.getMessage());");
+                }
+                methodBody.AppendLine("}");
+
+                methodBody.AppendLine();
+
+                //return
+                builder.AppendLine(Helpers.AddIndentation(methodBody.ToString(),
+                    1));
+            }
+            builder.AppendLine("}");
+            methodBody.Clear();
+
+            //generate the get to get all data
+            builder.AppendLine($"public static void get{table.Name}(Callback<List<{table.SingularName}>> callback)");
+            builder.AppendLine("{");
+            {
+                //declaration
+                methodBody.AppendLine($"List<{table.SingularName}> {table.LowerCaseName};");
+                methodBody.AppendLine($"{table.SingularName}Service service;");
+                methodBody.AppendLine($"Call<List<{table.SingularName}>> call;");
+                methodBody.AppendLine();
+
+                //initialization
+                methodBody.AppendLine($"{table.LowerCaseName} = null;");
+                methodBody.AppendLine();
+
+                methodBody.AppendLine($"service = RetrofitInstance.GetRetrofitInstance().create({table.SingularName}Service.class);");
+
+                //try
+                methodBody.AppendLine("try");
+                methodBody.AppendLine("{");
+                {
+                    //get data from server
+                    methodBody.AppendLine($"\tcall = service.getUsers();");
+                    methodBody.AppendLine($"\tget{table.Name}(call, callback);");
+                }
+                methodBody.AppendLine("}");
+
+                //catch
+                methodBody.AppendLine("catch(Exception ee)");
+                methodBody.AppendLine("{");
+                {
+                    methodBody.AppendLine($"\tSystem.out.println(ee.getMessage());");
+                }
+                methodBody.AppendLine("}");
+
+                methodBody.AppendLine();
+
+                builder.AppendLine(Helpers.AddIndentation(methodBody.ToString(),
+                    1));
+            }
+            builder.AppendLine("}");
+
+            return builder.ToString();
+        }
+
         private string GenerateApiInterface(Table table)
         {
             StringBuilder builder;
@@ -239,7 +324,7 @@ namespace DatabaseFunctionsGenerator.Java
             builder.AppendLine("import retrofit2.Retrofit;");
             builder.AppendLine("import retrofit2.converter.gson.GsonConverterFactory;");
             builder.AppendLine("import retrofit2.http.GET;");
-            builder.AppendLine("import retrofit2.http.Path;");
+            builder.AppendLine("import retrofit2.http.Query;");
 
             builder.AppendLine(GenerateApiInterface(table));
 
@@ -248,6 +333,7 @@ namespace DatabaseFunctionsGenerator.Java
             {
                 classBuilder.AppendLine(GenerateSyncGetMethod(table));
                 classBuilder.AppendLine(GenerateSyncDedicatedGetMethod(table));
+                classBuilder.AppendLine(GenerateAsyncGetMethod(table));
 
                 builder.AppendLine(Helpers.AddIndentation(classBuilder.ToString(),
                         1));
