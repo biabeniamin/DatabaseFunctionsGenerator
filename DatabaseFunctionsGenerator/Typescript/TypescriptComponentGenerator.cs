217f24529ed47c2081b5a599ee15d53094d517fe
biabeniamin <biabeniamin@gmail.com>
2018-10-24 22:47:19 +0300

    separeted class to generate typescript component

--- DatabaseFunctionsGenerator/Typescript/TypescriptComponentGenerator.cs
+++ DatabaseFunctionsGenerator/Typescript/TypescriptComponentGenerator.cs
@@ -17,104 +17,23 @@ namespace DatabaseFunctionsGenerator
             _database = database;
         }
 
-        private string GenerateAddEventHandler(Table table)
+        private void GenerateComponentForTable(Table table, string path)
         {
-            StringBuilder builder = new StringBuilder();
-            StringBuilder functionBody = new StringBuilder();
+            string tableComponentPath;
 
-            builder.AppendLine($"add{table.SingularName}(event)");
-            builder.AppendLine("{");
-            {
-                functionBody.AppendLine("event.preventDefault();");
-                functionBody.AppendLine("const target = event.target;");
-                functionBody.AppendLine($"let {table.LowerCaseSingularName} = {table.SingularName}Service.GetDefault{table.SingularName}();");
-                foreach (Column column in table.EditableColumns)
-                {
-                    functionBody.Append($"{table.LowerCaseSingularName}.{Helpers.GetLowerCaseString(column.Name)} = target.querySelector('");
-                    if (column.Type.IsForeignKey)
-                    {
-                        functionBody.Append($"#{column.Name}DropDown");
-                    }
-                    else
-                    {
-                        functionBody.Append($"#{column.Name}");
-                    }
+            tableComponentPath = $"{path}\\{table.LowerCaseSingularName}";
 
-                    functionBody.AppendLine($"').value;");
-                }
+            Directory.CreateDirectory(tableComponentPath);
 
-                foreach (Table parentTable in table.Parents)
-                {
-                    foreach (Column column in parentTable.Columns)
-                    {
-                        /* tableBody.AppendLine("\t<td>");
-                         {
-                             tableBody.AppendLine($"\t\t{{{{{table.LowerCaseSingularName}.{parentTable.LowerCaseSingularName}.{Helpers.GetLowerCaseString(column.Name)}}}}}");
-                         }
-                         tableBody.AppendLine("\t</td>");*/
-                    }
-                }
-
-                functionBody.AppendLine($"console.log({table.LowerCaseSingularName});");
-                functionBody.AppendLine($"this.{table.LowerCaseSingularName}Service.Add{table.SingularName}({table.LowerCaseSingularName});");
-
-                builder.Append(Helpers.AddIndentation(functionBody.ToString(), 1));
-            }
-            builder.AppendLine("}");
-
-            return builder.ToString();
-        }
-
-        private string GenerateDropDownChangeEventHandler(Table table)
-        {
-            StringBuilder builder = new StringBuilder();
-            StringBuilder functionBody = new StringBuilder();
-
-            builder.AppendLine($"{table.LowerCaseSingularName}Changed(event)");
-            builder.AppendLine("{");
-            {
-                functionBody.AppendLine($"console.log(event);");
-
-                builder.AppendLine(Helpers.AddIndentation(functionBody.ToString(), 1));
-            }
-            builder.AppendLine("}");
-
-            return builder.ToString();
-        }
-
-
-        private string GenerateViewForTable(string path, Table table)
-        {
-            StringBuilder builder = new StringBuilder();
-
-            builder.AppendLine($"import {{ {table.Name} }} from '../app/{table.Name}'");
-            builder.AppendLine($"import {{ {table.SingularName}Service }} from './{table.SingularName}Service'");
-
-            builder.AppendLine(GenerateAddEventHandler(table));
-
-            foreach (Table parentTable in table.Parents)
-            {
-                builder.AppendLine(GenerateDropDownChangeEventHandler(parentTable));
-            }
-
-            Helpers.WriteFile($"{path}\\{table.SingularName}.component.ts",
-                builder.ToString());
-
-            return builder.ToString();
+            TypescriptControllerComponentGenerator.GenerateControllerComponentForTable(table, path);
+            TypescriptComponentSpecGenerator.GenerateComponentSpec(table, tableComponentPath);
         }
 
         public void Generate(string path)
         {
             foreach (Table table in _database.Tables)
             {
-                string tableComponentPath;
-
-                tableComponentPath = $"{path}\\{table.LowerCaseSingularName}";
-
-                Directory.CreateDirectory(tableComponentPath);
-
-                GenerateViewForTable(path, table);
-                TypescriptComponentSpecGenerator.GenerateComponentSpec(table, tableComponentPath);
+                GenerateComponentForTable(table, path);
             }
         }
     }
---
+++ DatabaseFunctionsGenerator/Typescript/TypescriptControllerComponentGenerator.cs
@@ -0,0 +1,100 @@
+﻿using DatabaseFunctionsGenerator.Typescript;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseFunctionsGenerator
+{
+    public class TypescriptControllerComponentGenerator
+    {
+        private static string GenerateAddEventHandler(Table table)
+        {
+            StringBuilder builder = new StringBuilder();
+            StringBuilder functionBody = new StringBuilder();
+
+            builder.AppendLine($"add{table.SingularName}(event)");
+            builder.AppendLine("{");
+            {
+                functionBody.AppendLine("event.preventDefault();");
+                functionBody.AppendLine("const target = event.target;");
+                functionBody.AppendLine($"let {table.LowerCaseSingularName} = {table.SingularName}Service.GetDefault{table.SingularName}();");
+                foreach (Column column in table.EditableColumns)
+                {
+                    functionBody.Append($"{table.LowerCaseSingularName}.{Helpers.GetLowerCaseString(column.Name)} = target.querySelector('");
+                    if (column.Type.IsForeignKey)
+                    {
+                        functionBody.Append($"#{column.Name}DropDown");
+                    }
+                    else
+                    {
+                        functionBody.Append($"#{column.Name}");
+                    }
+
+                    functionBody.AppendLine($"').value;");
+                }
+
+                foreach (Table parentTable in table.Parents)
+                {
+                    foreach (Column column in parentTable.Columns)
+                    {
+                        /* tableBody.AppendLine("\t<td>");
+                         {
+                             tableBody.AppendLine($"\t\t{{{{{table.LowerCaseSingularName}.{parentTable.LowerCaseSingularName}.{Helpers.GetLowerCaseString(column.Name)}}}}}");
+                         }
+                         tableBody.AppendLine("\t</td>");*/
+                    }
+                }
+
+                functionBody.AppendLine($"console.log({table.LowerCaseSingularName});");
+                functionBody.AppendLine($"this.{table.LowerCaseSingularName}Service.Add{table.SingularName}({table.LowerCaseSingularName});");
+
+                builder.Append(Helpers.AddIndentation(functionBody.ToString(), 1));
+            }
+            builder.AppendLine("}");
+
+            return builder.ToString();
+        }
+
+        private static string GenerateDropDownChangeEventHandler(Table table)
+        {
+            StringBuilder builder = new StringBuilder();
+            StringBuilder functionBody = new StringBuilder();
+
+            builder.AppendLine($"{table.LowerCaseSingularName}Changed(event)");
+            builder.AppendLine("{");
+            {
+                functionBody.AppendLine($"console.log(event);");
+
+                builder.AppendLine(Helpers.AddIndentation(functionBody.ToString(), 1));
+            }
+            builder.AppendLine("}");
+
+            return builder.ToString();
+        }
+
+
+        public static string GenerateControllerComponentForTable(Table table, string path)
+        {
+            StringBuilder builder = new StringBuilder();
+
+            builder.AppendLine($"import {{ {table.Name} }} from '../app/{table.Name}'");
+            builder.AppendLine($"import {{ {table.SingularName}Service }} from './{table.SingularName}Service'");
+
+            builder.AppendLine(GenerateAddEventHandler(table));
+
+            foreach (Table parentTable in table.Parents)
+            {
+                builder.AppendLine(GenerateDropDownChangeEventHandler(parentTable));
+            }
+
+            Helpers.WriteFile($"{path}\\{table.SingularName}.component.ts",
+                builder.ToString());
+
+            return builder.ToString();
+        }
+
+    }
+}
