e992aff9241c296840873cff421cc0041aa369c7
biabeniamin <biabeniamin@gmail.com>
2018-11-22 21:55:08 +0200

    json import button into ui and working

--- DatabaseFunctionsGenerator/MainWindow.xaml.cs
+++ DatabaseFunctionsGenerator/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
@@ -20,11 +21,19 @@ namespace DatabaseFunctionsGenerator
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, INotifyPropertyChanged
     {
         private Database _database;
         private Table _selectedTable;
-        private DelegateCommand _generateDelegateCommand;   
+        private DelegateCommand _generateDelegateCommand;
+        private DelegateCommand _importDelegateCommand;
+
+        public DelegateCommand ImportDelegateCommand
+        {
+            get { return _importDelegateCommand; }
+            set { _importDelegateCommand = value; }
+        }
+        
 
         public DelegateCommand GenerateDelegateCommand
         {
@@ -59,7 +68,7 @@ namespace DatabaseFunctionsGenerator
 
             Database = new Database();
 
-            Database.Tables.Add(new Table("Users"));
+           /* Database.Tables.Add(new Table("Users"));
             Database.Tables.Add(new Table("VitalSigns"));
             Database.Tables.Add(new Table("Locations"));
             Database.Tables.Add(new Table("Administrators"));
@@ -91,19 +100,58 @@ namespace DatabaseFunctionsGenerator
 
             Database.Tables[0].DedicatedGetRequests.Add(new DedicatedGetRequest(Database.Tables[0].Columns[0], Database.Tables[0].Columns[1]));
             Database.Tables[0].DedicatedGetRequests.Add(new DedicatedGetRequest(Database.Tables[0].Columns[2]));
-            Database.Tables[1].DedicatedGetRequests.Add(new DedicatedGetRequest(Database.Tables[1].Columns[0]));
+            Database.Tables[1].DedicatedGetRequests.Add(new DedicatedGetRequest(Database.Tables[1].Columns[0]));*/
+            /*Database.Tables.Add(new Table("Users"));
+            Database.Tables.Add(new Table("Locations"));
+            Database.Tables.Add(new Table("AccessLogs"));
+            Database.Tables.Add(new Table("Doors"));
+            Database.Tables.Add(new Table("Pins"));
+            Database.Tables.Add(new Table("Empty"));
+
+            //            Database.Tables[0].Columns.Add(new Column("Id", new ColumnType(Types.Integer, true, true)));
+            Database.Tables[0].Columns.Add(new Column("Username", new ColumnType(Types.Varchar, 20)));
+            Database.Tables[0].Columns.Add(new Column("Age", new ColumnType(Types.Integer)));
+            Database.Tables[0].Columns.Add(new Column("Description", new ColumnType(Types.Text)));
+            Database.Tables[0].Columns.Add(new Column("CNP", new ColumnType(Types.Varchar, 20)));
+            Database.Tables[0].Columns.Add(new Column("DateOfBirth", new ColumnType(Types.DateTime)));
+
+            Database.Tables[2].Columns.Add(new Column("CardValue", new ColumnType(Types.Varchar, 20)));
+
+            Database.Tables[3].Columns.Add(new Column("Name", new ColumnType(Types.Varchar, 20)));
+
+            Database.Tables[4].Columns.Add(new Column("Pin", new ColumnType(Types.Varchar, 20)));
 
 
-            SelectedTable = Database.Tables[0];
+            Database.Relations.Add(new Relation(Database.Tables[0], Database.Tables[2], RelationType.OneToMany));
+            Database.Relations.Add(new Relation(Database.Tables[3], Database.Tables[2], RelationType.OneToMany));
+            Database.Relations.Add(new Relation(Database.Tables[2], Database.Tables[4], RelationType.OneToMany));
+            Database.Relations.Add(new Relation(Database.Tables[4], Database.Tables[5], RelationType.OneToMany));*/
+
+
+            //SelectedTable = Database.Tables[0];
 
             Generator generator = new Generator(_database);
             generator.Generate();
 
             _generateDelegateCommand = new DelegateCommand(GenerateCommand);
-            
+
+            _importDelegateCommand = new DelegateCommand(ImportFromJsonCommand);
+
+
         }
 
-        
+        private void ImportFromJsonCommand()
+        {
+            OpenFileDialog openFileDialog;
+
+            openFileDialog = new OpenFileDialog();
+            openFileDialog.FileName = "Config.json";
+
+            if (true == openFileDialog.ShowDialog())
+            {
+                Database = Database.ImportFromJson(Helpers.ReadFile(openFileDialog.FileName));
+            }
+        }
 
         private void GenerateCommand()
         {
