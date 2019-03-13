bb5d2a30e97e996c7e61d42e764d0b6462e9f8e5
biabeniamin <biabeniamin@outlook.com>
2019-03-13 15:14:51 +0200

    deployer class added

---
+++ DatabaseFunctionsGenerator/Deployer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseFunctionsGenerator
+{
+    public class Deployer
+    {
+        private Generator _generator;
+        
+        public Deployer(Generator generator)
+        {
+            _generator = generator;
+        }
+    }
+}
