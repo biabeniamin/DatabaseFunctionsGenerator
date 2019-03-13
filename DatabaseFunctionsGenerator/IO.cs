fb0fae71d7126f5a4de2f290ee7d3642350965e6
biabeniamin <biabeniamin@outlook.com>
2019-03-13 15:36:07 +0200

    io static class defined

---
+++ DatabaseFunctionsGenerator/IO.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Hosting;
+
+namespace DatabaseFunctionsGenerator
+{
+    public static class IO
+    {
+        public static string GetPath(string path)
+        {
+            //this is designed to determine if it is running on a webserver or desktop
+            string relativePath = path;
+
+            if (!Path.IsPathRooted(path) && HttpRuntime.AppDomainAppId != null)
+            {
+                relativePath = Path.Combine(@HostingEnvironment.ApplicationPhysicalPath, $"bin\\{path}");
+            }
+
+            return relativePath;
+        }
+
+        public static void CreateDirectory(string path)
+        {
+            Directory.CreateDirectory(GetPath(path));
+        }
+
+        public static bool DoesDirectoryExists(string path)
+        {
+            return Directory.Exists(GetPath(path));
+        }
+
+        public static string ReadFile(string path)
+        {
+            String text;
+            StreamReader reader;
+
+            reader = new StreamReader(GetPath(path));
+            text = "";
+
+            text = reader.ReadToEnd();
+
+            reader.Dispose();
+
+            return text;
+        }
+
+        public static void WriteFile(string path, string text)
+        {
+            StreamWriter writer;
+
+            writer = new StreamWriter(GetPath(path));
+
+            writer.Write(text);
+
+            writer.Dispose();
+        }
+    }
+}
