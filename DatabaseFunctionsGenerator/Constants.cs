8323491f70a9cce8f39d604a4ab6bf0e26f9aacb
biabeniamin <biabeniamin@outlook.com>
2019-03-15 14:25:54 +0200

    constants class defined

---
+++ DatabaseFunctionsGenerator/Constants.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseFunctionsGenerator
+{
+    public static class Constants
+    {
+        public static string MysqlServer => "localhost";
+        public static string MysqlDatabase => "gen";
+        public static string MysqlUsername => "root";
+        public static string MysqlPassword => "";
+    }
+}
