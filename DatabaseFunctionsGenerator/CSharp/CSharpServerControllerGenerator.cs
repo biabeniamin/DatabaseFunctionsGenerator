9c810a33cb6d96ffa6970ec550c1fed896836fef
biabeniamin <biabeniamin@outlook.com>
2019-05-11 15:00:44 +0300

    csharp controller get request works

--- DatabaseFunctionsGenerator/CSharp/CSharpServerControllerGenerator.cs
+++ DatabaseFunctionsGenerator/CSharp/CSharpServerControllerGenerator.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DatabaseFunctionsGenerator.CSharp
+namespace DatabaseFunctionsGenerator
 {
     public class CSharpServerControllerGenerator : IGenerator
     {
@@ -13,6 +13,46 @@ namespace DatabaseFunctionsGenerator.CSharp
         public CSharpServerControllerGenerator(Database database)
         {
             _database = database;
+        }
+
+        private string GenerateGetRequest(Table table)
+        {
+            StringBuilder builder;
+
+            builder = new StringBuilder();
+
+            builder.AppendLine($"// GET {table.LowerCaseName}/values");
+            builder.AppendLine($"public IEnumerable<{table.SingularName}> Get()");
+            builder.AppendLine("{");
+            {
+                StringBuilder blockBuilder = new StringBuilder();
+
+                blockBuilder.AppendLine($"MySqlDataReader reader = new DatabaseOperations().GetReader(\"SELECT * FROM {table.Name}\");");
+                blockBuilder.AppendLine($"List<{table.SingularName}> list = new List<{table.SingularName}>();");
+                blockBuilder.AppendLine();
+
+                blockBuilder.AppendLine("while(reader.Read())");
+                blockBuilder.AppendLine("{");
+                {
+                    blockBuilder.AppendLine($"\tlist.Add(new {table.SingularName}(");
+
+                    foreach(Column column in table.EditableColumns)
+                    {
+                        blockBuilder.AppendLine($"\t\t({column.Type.GetCSharpType()})reader[\"{column.Name}\"],");
+                    }
+                    Helpers.RemoveLastApparition(blockBuilder, ",");
+
+                    blockBuilder.AppendLine($"\t));");
+                }
+                blockBuilder.AppendLine("}");
+
+                blockBuilder.AppendLine("");
+                blockBuilder.AppendLine("return list;");
+                builder.Append(Helpers.AddIndentation(blockBuilder, 1));
+            }
+            builder.AppendLine("}");
+
+            return builder.ToString();
         }
 
         private void GenerateController(Table table, string path)
@@ -26,32 +66,24 @@ namespace DatabaseFunctionsGenerator.CSharp
             classBuilder = new StringBuilder();
 
             builder.AppendLine("//generated automatically");
-            builder.AppendLine("using Newtonsoft.Json;");
+            builder.AppendLine("using MySql.Data.MySqlClient;");
             builder.AppendLine("using System;");
             builder.AppendLine("using System.Collections.Generic;");
             builder.AppendLine("using System.Collections.ObjectModel;");
             builder.AppendLine("using System.Linq;");
-            builder.AppendLine("using System.Text;");
-            builder.AppendLine("using System.Threading.Tasks; ");
+            builder.AppendLine("using System.Net;");
+            builder.AppendLine("using System.Net.Http;");
 
 
             builder.AppendLine("namespace DatabaseFunctionsGenerator");
             builder.AppendLine("{");
             {
 
-                namespaceBuilder.AppendLine($"public class {table.SingularName}");
+                namespaceBuilder.AppendLine($"public class {table.SingularName}Controller : ApiController");
                 namespaceBuilder.AppendLine("{");
                 {
-                    classBuilder.AppendLine(GenerateFields(table));
-                    classBuilder.AppendLine(GenerateGettersSetters(table));
-                    classBuilder.AppendLine(GenerateConstructor(table));
-
-                    if (0 < table.Parents.Count)
-                    {
-                        classBuilder.AppendLine(GenerateConstructorWithParents(table));
-                    }
-
-                    classBuilder.AppendLine(GenerateEmptyConstructor(table));
+                    classBuilder.AppendLine(GenerateGetRequest(table));
+                    
 
                     namespaceBuilder.AppendLine(Helpers.AddIndentation(classBuilder.ToString(),
                         1));
