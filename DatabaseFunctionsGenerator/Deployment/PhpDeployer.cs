ca48d0bd781a35d0df55c171384df905ce6c38f9
biabeniamin <biabeniamin@outlook.com>
2019-06-04 20:50:58 +0300

    add php deployer class

---
+++ DatabaseFunctionsGenerator/Deployment/PhpDeployer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseFunctionsGenerator.Deployment
+{
+    public class PhpDeployer : IDeployer
+    {
+        private Generator _generator;
+
+        public PhpDeployer(Generator generator)
+        {
+            _generator = generator;
+        }
+
+        public void Deploy()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
