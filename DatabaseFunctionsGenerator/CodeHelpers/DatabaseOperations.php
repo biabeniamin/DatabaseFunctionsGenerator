82eee8cd4ff709e1ef3dcdeeca9a271695376ad5
biabeniamin <biabeniamin@outlook.com>
2019-06-04 21:08:47 +0300

    make static database name dynamical

--- DatabaseFunctionsGenerator/CodeHelpers/DatabaseOperations.php
+++ DatabaseFunctionsGenerator/CodeHelpers/DatabaseOperations.php
@@ -14,7 +14,7 @@ class DatabaseOperations
         //$this->connection = new mysqli("localhost", "avramian_biabeni", "Acetronic1","avramian_hs");
         //$this->connection = new mysqli("localhost", "pagespee_biabeniamin", "Acetronic1","pagespee_hard");
         //$this->connection = new mysqli("localhost", "pagespee_biabeniamin", "Acetronic1","pagespee_hs18");
-        $this->connection = new mysqli("localhost", "root", "","generator");
+        $this->connection = new mysqli("localhost", "root", "","!--databaseName--!");
         //$this->connection = new mysqli("localhost", "avramian_codefor", "Acetronic1","avramian_codeforgood18");
         if ($this->connection->connect_error)
         {
