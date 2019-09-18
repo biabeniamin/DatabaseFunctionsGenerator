dc966b85e614bdaf651ab2bb9afc4fc805774c0a
biabeniamin <biabeniamin@outlook.com>
2019-09-18 13:45:10 +0300

    import parent tables

--- DatabaseFunctionsGenerator/Python/PythonControllerGenerator.cs
+++ DatabaseFunctionsGenerator/Python/PythonControllerGenerator.cs
@@ -31,6 +31,9 @@ namespace DatabaseFunctionsGenerator.Python
             builder.AppendLine("from SqlAlchemy import Base");
             builder.AppendLine("from sqlalchemy.ext.declarative import declared_attr");
             builder.AppendLine("from sqlalchemy import *");
+            //import parent tables
+            foreach(Table parentTable in table.Parents)
+                builder.AppendLine($"from {parentTable.Name} import {parentTable.Name}");
 
             //class content
             {
