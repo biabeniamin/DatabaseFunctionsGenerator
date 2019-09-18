0f6aae68f14dc1725f15fc56e29eb9909fa22978
biabeniamin <biabeniamin@outlook.com>
2019-09-18 13:38:45 +0300

    add generate controller method

--- DatabaseFunctionsGenerator/Python/PythonControllerGenerator.cs
+++ DatabaseFunctionsGenerator/Python/PythonControllerGenerator.cs
@@ -15,9 +15,39 @@ namespace DatabaseFunctionsGenerator.Python
             _database = database;
         }
 
+        private void GenerateController(Table table, string path)
+        {
+            StringBuilder builder;
+            StringBuilder classBuilder;
+
+            builder = new StringBuilder();
+            classBuilder = new StringBuilder();
+
+            //class content
+            {
+                builder.AppendLine(Helpers.AddIndentation(classBuilder.ToString(),
+                     1));
+            }
+
+            builder.AppendLine("#generated automatically");
+
+            IO.WriteFile($"{path}\\{table.Name}.py", (builder.ToString()));
+
+            //return builder.ToString();
+        }
+
         public void Generate(string path)
         {
-            throw new NotImplementedException();
+            string controllersPath;
+
+            controllersPath = $"{path}\\Controllers";
+
+            IO.CreateDirectory(controllersPath);
+
+            foreach (Table table in _database.Tables)
+            {
+                GenerateController(table, controllersPath);
+            }
         }
     }
 }
