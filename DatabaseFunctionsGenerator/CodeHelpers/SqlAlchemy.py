c3276c31ae50e006109f9de193875a31a07a4618
biabeniamin <biabeniamin@outlook.com>
2019-09-18 15:14:18 +0300

    add sqlAlchemy file

---
+++ DatabaseFunctionsGenerator/CodeHelpers/SqlAlchemy.py
@@ -0,0 +1,8 @@
+﻿from sqlalchemy import *
+from sqlalchemy.ext.declarative import declarative_base, declared_attr
+Base = declarative_base()
+username="root"
+server="localhost"
+port=3306
+database="!--databaseName--!"
+engine = create_engine('mysql+mysqlconnector://%s@%s:%d/%s'%(username, server, port, database), echo=True)
