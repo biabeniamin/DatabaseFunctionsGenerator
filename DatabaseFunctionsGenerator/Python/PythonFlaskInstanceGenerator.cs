6ebacd9c778ca72289433e0981535b70a74f0572
biabeniamin <biabeniamin@outlook.com>
2019-09-18 15:25:31 +0300

    add imports for flask instance

--- DatabaseFunctionsGenerator/Python/PythonFlaskInstanceGenerator.cs
+++ DatabaseFunctionsGenerator/Python/PythonFlaskInstanceGenerator.cs
@@ -17,6 +17,19 @@ namespace DatabaseFunctionsGenerator.Python
 
         public void Generate(string path)
         {
+            StringBuilder builder = new StringBuilder();
+
+            //imports
+            builder.AppendLine("#generated automatically");
+            builder.AppendLine("import flask");
+            builder.AppendLine("import flask_restless");
+            builder.AppendLine("from sqlalchemy.ext.declarative import declarative_base, declared_attr");
+            builder.AppendLine("from sqlalchemy.orm import sessionmaker, scoped_session, validates");
+            builder.AppendLine("from sqlalchemy import *");
+            builder.AppendLine("from ValidationError import ValidationError");
+            builder.AppendLine("from SqlAlchemy import Base, engine");
+
+            IO.WriteFile($"{path}\\main.py", (builder.ToString()));
         }
     }
 }
