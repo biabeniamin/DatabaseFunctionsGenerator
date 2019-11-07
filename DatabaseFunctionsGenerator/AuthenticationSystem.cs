1ad694eeee811ce85b8253d86332e8c51e7da857
biabeniamin <biabeniamin@outlook.com>
2019-11-07 19:33:19 +0200

    add authentication system class

---
+++ DatabaseFunctionsGenerator/AuthenticationSystem.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseFunctionsGenerator
+{
+    class AuthenticationSystem
+    {
+        public Table GenerateAuthenticationTable()
+        {
+            Table table;
+            List<Column> columns;
+
+            columns = new List<Column>();
+
+            columns.Add(new Column("Value", new ColumnType(Types.Varchar, 40)));
+            columns.Add(new Column("Address", new ColumnType(Types.Varchar, 15)));
+            columns.Add(new Column("LastUpdate", new ColumnType(Types.Varchar, 15)));
+
+            table = new Table("Notifications", columns);
+
+            return table;
+        }
+    }
+}
