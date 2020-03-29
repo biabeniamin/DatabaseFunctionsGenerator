92b6e0808093f9838507e2a3781b8dfd95720dfc
biabeniamin <biabeniamin@outlook.com>
2020-03-29 17:49:07 +0300

    add convert to json function

--- DatabaseFunctionsGenerator/CodeHelpers/SqlAlchemy.py
+++ DatabaseFunctionsGenerator/CodeHelpers/SqlAlchemy.py
@@ -1,6 +1,9 @@
 ﻿from sqlalchemy import *
 from sqlalchemy.ext.declarative import declarative_base, declared_attr
 from sqlalchemy import inspect
+import json
+import datetime
+
 Base = declarative_base()
 username="root"
 server="localhost"
@@ -12,3 +15,16 @@ def object_as_dict(obj):
     return {c.key: getattr(obj, c.key)
             for c in inspect(obj).mapper.column_attrs}
 
+def alchemyencoder(obj):
+    """JSON encoder function for SQLAlchemy special classes."""
+    if isinstance(obj, datetime.date):
+        return obj.isoformat()
+    elif isinstance(obj, decimal.Decimal):
+        return float(obj)
+
+def convertToJson(data):
+	objects = []
+	for row in data:
+		objects.append(object_as_dict(row))
+
+	return json.dumps(objects, default = alchemyencoder)
