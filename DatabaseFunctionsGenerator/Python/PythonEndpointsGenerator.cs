8447f0fdbdabe3c6b7a23c2071148a318fabe190
biabeniamin <biabeniamin@outlook.com>
2020-03-30 21:34:30 +0300

    generate get endpoint

--- DatabaseFunctionsGenerator/Python/PythonEndpointsGenerator.cs
+++ DatabaseFunctionsGenerator/Python/PythonEndpointsGenerator.cs
@@ -15,6 +15,25 @@ namespace DatabaseFunctionsGenerator.Python
             _database = database;
         }
 
+
+        private string GenerateGetEndpoint(Table table)
+        {
+            StringBuilder builder;
+            StringBuilder function;
+
+            builder = new StringBuilder();
+            function = new StringBuilder();
+
+            builder.AppendLine("#get endpoint");
+
+            builder.AppendLine($"def get(self):");
+            function.AppendLine($"return {table.Name}.get{table.Name}(session)");
+
+            builder.AppendLine(Helpers.AddIndentation(function, 1));
+
+            return builder.ToString();
+        }
+
         private string GenerateAPIEndpoints(Table table)
         {
             StringBuilder builder;
@@ -22,6 +41,7 @@ namespace DatabaseFunctionsGenerator.Python
             builder = new StringBuilder();
 
             builder.AppendLine("#API endpoints");
+            builder.AppendLine(GenerateGetEndpoint(table));
 
             return builder.ToString();
         }
@@ -36,6 +56,9 @@ namespace DatabaseFunctionsGenerator.Python
 
             //imports
             builder.AppendLine("#generated automatically");
+            builder.AppendLine("from flask_restful import Resource");
+            builder.AppendLine("from SqlAlchemyMain import *");
+            builder.AppendLine($"import {table.Name}");
 
             builder.AppendLine($"class {table.SingularName}List(Resource):");
             //endpoints
