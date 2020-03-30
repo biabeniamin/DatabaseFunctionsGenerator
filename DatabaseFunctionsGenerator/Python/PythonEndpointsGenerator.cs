74549b8b72ce96465d69cdadfb0b1c591260b55c
biabeniamin <biabeniamin@outlook.com>
2020-03-30 21:23:09 +0300

    fix brackets in python endpoints

--- DatabaseFunctionsGenerator/Python/PythonEndpointsGenerator.cs
+++ DatabaseFunctionsGenerator/Python/PythonEndpointsGenerator.cs
@@ -8,40 +8,39 @@ namespace DatabaseFunctionsGenerator.Python
 {
     class PythonEndpointsGenerator : IGenerator
     {
-            private Database _database;
+        private Database _database;
 
-            public PythonEndpointsGenerator(Database database)
+        public PythonEndpointsGenerator(Database database)
+        {
+            _database = database;
+        }
+
+
+        private void GenerateController(Table table, string path)
+        {
+            StringBuilder builder;
+
+            builder = new StringBuilder();
+
+            //imports
+            builder.AppendLine("#generated automatically");
+
+            IO.WriteFile($"{path}\\{table.SingularName}Endpoints.py", (builder.ToString()));
+        }
+
+        public void Generate(string path)
+        {
+            string endpointsPath;
+
+            endpointsPath = $"{path}";
+
+            IO.CreateDirectory(endpointsPath);
+
+            foreach (Table table in _database.Tables)
             {
-                _database = database;
-            }
-
-
-            private void GenerateController(Table table, string path)
-            {
-                StringBuilder builder;
-
-                builder = new StringBuilder();
-
-                //imports
-                builder.AppendLine("#generated automatically");
-
-                IO.WriteFile($"{path}\\{table.SingularName}Endpoints.py", (builder.ToString()));
-            }
-
-            public void Generate(string path)
-            {
-                string endpointsPath;
-
-                endpointsPath = $"{path}";
-
-                IO.CreateDirectory(endpointsPath);
-
-                foreach (Table table in _database.Tables)
-                {
-                    GenerateController(table, endpointsPath);
-                }
+                GenerateController(table, endpointsPath);
             }
         }
     }
-
 }
+
