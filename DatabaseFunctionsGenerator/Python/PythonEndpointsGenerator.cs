a43d0c321cd82a6d987f15a4168f0aa4119217ca
biabeniamin <biabeniamin@outlook.com>
2020-03-30 21:20:50 +0300

    create python endpoints class

---
+++ DatabaseFunctionsGenerator/Python/PythonEndpointsGenerator.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseFunctionsGenerator.Python
+{
+    class PythonEndpointsGenerator : IGenerator
+    {
+            private Database _database;
+
+            public PythonEndpointsGenerator(Database database)
+            {
+                _database = database;
+            }
+
+
+            private void GenerateController(Table table, string path)
+            {
+                StringBuilder builder;
+
+                builder = new StringBuilder();
+
+                //imports
+                builder.AppendLine("#generated automatically");
+
+                IO.WriteFile($"{path}\\{table.SingularName}Endpoints.py", (builder.ToString()));
+            }
+
+            public void Generate(string path)
+            {
+                string endpointsPath;
+
+                endpointsPath = $"{path}";
+
+                IO.CreateDirectory(endpointsPath);
+
+                foreach (Table table in _database.Tables)
+                {
+                    GenerateController(table, endpointsPath);
+                }
+            }
+        }
+    }
+
+}
