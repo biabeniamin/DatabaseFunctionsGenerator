d505a0f4d85cab27ddcb00c290a1600d3b4c108f
biabeniamin <biabeniamin@outlook.com>
2020-03-31 15:14:09 +0300

    remove unused parameter from dict_as_obj function

--- DatabaseFunctionsGenerator/CodeHelpers/SqlAlchemy.py
+++ DatabaseFunctionsGenerator/CodeHelpers/SqlAlchemy.py
@@ -15,7 +15,7 @@ def object_as_dict(obj):
     return {c: getattr(obj, c)
             for c in obj.__dict__.keys() if c[0]!='_'}
 
-def dict_as_obj(args, loc2, obj):
+def dict_as_obj(args, obj):
 	loc = obj()
 	for arg in args:
 		setattr(loc, arg, args[arg])
--- DatabaseFunctionsGenerator/Python/PythonEndpointsGenerator.cs
+++ DatabaseFunctionsGenerator/Python/PythonEndpointsGenerator.cs
@@ -47,7 +47,7 @@ namespace DatabaseFunctionsGenerator.Python
             builder.AppendLine($"def post(self):");
             function.AppendLine($"requestedArgs = getArguments(['{table.ToString("', '", true)}'])");
             function.AppendLine($"args  = requestedArgs.parse_args()");
-            function.AppendLine($"{table.LowerCaseSingularName}  = dict_as_obj(args, 0, {table.SingularName}.{table.SingularName})");
+            function.AppendLine($"{table.LowerCaseSingularName}  = dict_as_obj(args, {table.SingularName}.{table.SingularName})");
             function.AppendLine($"{table.SingularName}.add{table.SingularName}(session, {table.LowerCaseSingularName})");
             function.AppendLine($"return {table.SingularName}.get{table.Name}(session)");
 
