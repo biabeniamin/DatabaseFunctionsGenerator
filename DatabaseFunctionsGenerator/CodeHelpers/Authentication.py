3f2c36a7fae3c779103fb70535f1ac8384012ae5
biabeniamin <biabeniamin@outlook.com>
2020-04-02 21:51:55 +0300

    add authentication file for python

---
+++ DatabaseFunctionsGenerator/CodeHelpers/Authentication.py
@@ -0,0 +1,14 @@
+﻿import Token
+import TokenUser
+
+def checkToken(session):
+	print("checking token")
+	from FlaskRestfulHelpers import getArguments
+	requestedArgs = getArguments(['token'])
+	args  = requestedArgs.parse_args()
+	print(args)
+	token = Token.getTokensByValue(session, args['token'])
+	if len(token) == 0:
+		print('invalid token')
+	print(token)
+	return [] 
