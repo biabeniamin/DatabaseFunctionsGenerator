a1704a0f6c6283bc401116e3acc00adb45e4f77f
biabeniamin <biabeniamin@outlook.com>
2020-04-08 21:45:16 +0300

    add token authentication endpoints file

---
+++ DatabaseFunctionsGenerator/CodeHelpers/TokenAuthenticationEndpoints.py
@@ -0,0 +1,11 @@
+﻿#generated automatically
+from flask_restful import Resource
+import Authentication
+class TokenAuthenticationEndpoints(Resource):
+	def __init__(self, **kwargs):
+		self.session = kwargs['session']
+	
+	#API endpoints
+	#post endpoint
+	def post(self):
+		return Authentication.login(self.session)  
