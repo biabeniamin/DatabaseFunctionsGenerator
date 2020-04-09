5a26f4cbce876fe07134318fb55d4faec9add291
biabeniamin <biabeniamin@outlook.com>
2020-04-09 10:12:34 +0300

    simplify convertToJson function
    remove code handled by alchemyencoder

--- DatabaseFunctionsGenerator/CodeHelpers/SqlAlchemy.py
+++ DatabaseFunctionsGenerator/CodeHelpers/SqlAlchemy.py
@@ -41,16 +41,6 @@ def alchemyencoder(obj):
 
 def convertToJson(data):
 	if isinstance(data, dict):
-		for key in data:
-			requireTrans = 0
-			if isinstance(data[key], list) and len(data[key]) > 0:
-				if isinstance(data[key][0], Base):
-					requireTrans = 1
-			elif isinstance(data[key], Base):
-				requireTrans = 1
-
-			if requireTrans > 0:
-				data[key]=object_as_dict(data[key])
 		return json.dumps(data, default = alchemyencoder)
 
 	return json.dumps(object_as_dict(data), default = alchemyencoder)
