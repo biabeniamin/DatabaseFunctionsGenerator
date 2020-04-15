67bc73287cc90ed80b4c68c7fb8c03b98deb4802
biabeniamin <biabeniamin@outlook.com>
2020-04-15 17:07:31 +0300

    return login status in python authentication login

--- DatabaseFunctionsGenerator/CodeHelpers/Authentication.py
+++ DatabaseFunctionsGenerator/CodeHelpers/Authentication.py
@@ -8,11 +8,11 @@ def login(session, username, password, remote_addr):
 	tokenUser = TokenUser.getTokenUsersByUsernamePassword(session, username, password)
 	if len(tokenUser) == 0:
 		print("invalid credentials")
-		return {'error' : 'Invalid credentials'}
+		return {'error' : 'Invalid credentials'}, 0
 
 	token = Token.Token(value=str(uuid.uuid4()), address = remote_addr, lastUpdate = datetime.utcnow(), tokenUserId = tokenUser[0].tokenUserId)
 	Token.addToken(session, token)
-	return [token]
+	return token, 1
 
 def checkToken(session, token, remote_addr):
 	isAuthorized = 1
