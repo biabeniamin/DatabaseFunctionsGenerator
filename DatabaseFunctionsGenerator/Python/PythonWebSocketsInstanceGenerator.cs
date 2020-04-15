e1ea1db91f5f5881d4efbc720802d7edc9369948
biabeniamin <biabeniamin@outlook.com>
2020-04-15 11:12:42 +0300

    add python websockets instance generator class

---
+++ DatabaseFunctionsGenerator/Python/PythonWebSocketsInstanceGenerator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseFunctionsGenerator.Python
+{
+    class PythonWebSocketsInstanceGenerator : IGenerator
+    {
+        private Database _database;
+
+        public PythonWebSocketsInstanceGenerator(Database database)
+        {
+            _database = database;
+        }
+
+        
+
+        public void Generate(string path)
+        {
+            StringBuilder builder = new StringBuilder();
+
+            //imports
+            builder.AppendLine("#generated automatically");
+
+            IO.WriteFile($"{path}\\WebSockets.py", (builder.ToString()));
+        }
+    }
+}
